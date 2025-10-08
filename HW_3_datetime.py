1e110580ec06bcd67a4657ab222bd723d27dde7d
Tetiana Napolskykh <NapolskykhTetiana@gmail.com>
2025-10-08 09:33:34 +0200

    Add HW_3

--- HW_3_datetime.py
+++ HW_3_datetime.py
@@ -1,19 +1,3 @@
-from datetime import datetime #Imported the datetime module.
-
-def get_days_from_today(date): 
-
-    try:
-        target_data=datetime.strptime(date, "%Y-%m-%d").date() #Converted the date string in 'YYYY-MM-DD' format into a datetime object.
-        today=datetime.today().date() #Got the current date using datetime.today().
-        delta=today-target_data #Calculated the difference between the current date and the given date.
-        return delta.days #Returned the difference in days as an integer.
-
-    except ValueError:
-        return ValueError ("Incorrect date format. Expected 'YYYY-MM-DD'")
-    
-print(get_days_from_today("2025-09-28"))
-print(get_days_from_today("1985-05-30"))
-
 """
 Create a function get_days_from_today(date)
  that calculates the number of days between 
